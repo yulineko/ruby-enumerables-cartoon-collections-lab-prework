b9dce86e83efe996d76f72795a3092343f3239d9
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-08 20:20:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves) # code an argument here
   # Your code here
-  dwarves.each_with_index 
+  dwarves.each_with_index do 
 end
 
 def summon_captain_planet# code an argument here
