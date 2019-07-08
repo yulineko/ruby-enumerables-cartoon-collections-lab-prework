767359c6908c47c48ec450a12e4026d84efb269f
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-08 20:32:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,5 +23,5 @@ def find_the_cheese(snacks) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks.find do |element|  
-    
+    cheese_types.include?
 end
