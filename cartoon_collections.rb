ec3d3f4786b2a98106f875381977898d2b820994
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-08 20:26:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves) # code an argument here
   end
 end
 
-def summon_captain_planet() # code an argument here
+def summon_captain_planet(planeteer_calls) # code an argument here
   # Your code here
   
 end
