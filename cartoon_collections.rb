fcd39f8fc629481b46c60d4af362fcd9b217e6e0
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-08 20:26:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(planeteer_calls) # code an argument here
   # Your code here
-  planeteer_calls.map 
+  planeteer_calls.map do ||
 end
 
 def long_planeteer_calls# code an argument here
